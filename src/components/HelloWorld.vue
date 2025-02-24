d45cd2d1308a34cd2dc874923768e5329425a7cc
LiuKun <308880147@qq.com>
2025-02-24 17:34:17 +0800

    下载方法测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <button @click="downloadFile(url)">测试</button>
   </div>
 </template>
 
@@ -88,26 +9,44 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      url: 'https://ogliu-images.oss-cn-guangzhou.aliyuncs.com/blog/20201228114246.png'
+    }
+  },
+  methods: {
+    // 文件下载
+    downloadFile (url) {
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('网络错误或文件不存在')
+          }
+          return response.blob()
+        })
+        .then(blob => {
+          // 创建下载链接
+          const downloadUrl = URL.createObjectURL(blob)
+
+          // 创建隐藏的a标签
+          const a = document.createElement('a')
+          a.href = downloadUrl
+          a.download = 'download' // 下载时的文件名，可以自定义
+
+          // 将a标签插入DOM
+          document.body.appendChild(a)
+
+          // 触发点击事件
+          a.click()
+
+          // 移除a标签
+          document.body.removeChild(a)
+
+          // 释放URL对象
+          URL.revokeObjectURL(downloadUrl)
+        })
+        .catch(error => {
+          console.error('下载失败:', error)
+        })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
