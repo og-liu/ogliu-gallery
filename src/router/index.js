a0b5389e1d5c1a8db93afed65ddd8dcd9b104185
LiuKun <308880147@qq.com>
2025-02-28 13:05:29 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Entry from '@/view/entry.vue'
-import Gallery from '@/components/Gallery.vue'
 
 Vue.use(Router)
+
+// 修改为路由懒加载（动态导入）：
+const Entry = () => import(/* webpackChunkName: "Entry" */ '@/view/entry.vue')
+const Gallery = () => import(/* webpackChunkName: "Gallery" */ '@/components/Gallery.vue')
+
+// 对于特殊依赖的异步加载（可选）
+// const HeavyComponent = resolve => require.ensure([], () => resolve(
+//   require('../views/HeavyComponent.vue'),
+//   'heavy' // webpack chunk 命名
+// ))
 
 export default new Router({
   routes: [
