7de26d29cf6b0e41186588073976511679e2a0ae
LiuKun <308880147@qq.com>
2025-03-01 17:10:30 +0800

    测试

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,16 @@ const A062 = () => import(/* webpackChunkName: "index" */ '@/view/sku/A062.vue')
 
 export default new Router({
   routes: [
-    { path: '/', name: 'image', component: image },
+    // { path: '/A0239/这是一段安全的图片提取链接，请放心前往下载，点击发货内容旁边的复制图标，快速复制全部内容,前往浏览器粘贴即可', name: 'image', component: image },
+
     { path: '/A034&yilmale', name: 'A034', component: A034 },
     { path: '/A035&nduamld', name: 'A035', component: A035 },
     { path: '/A056&vmaoish', name: 'A056', component: A056 },
-    { path: '/A062&ndjw', name: 'A062', component: A062 }
+    { path: '/A062&ndjw', name: 'A062', component: A062 },
+    {
+      path: '*',
+      name: '404',
+      component: image
+    }
   ]
 })
